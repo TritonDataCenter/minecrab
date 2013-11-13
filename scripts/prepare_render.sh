2a375d5d9660cfb89ba2bb503ca03bf31d9d24c7
Drew Miller <drew666@gmail.com>
2013-11-12 16:43:18 -0800

    config file

--- scripts/prepare_render.sh
+++ scripts/prepare_render.sh
@@ -18,10 +18,12 @@ case $(uname -s) in
     ;;
   "SunOS" )
     echo "Solaris";
+    DEST="/minecraft/versions/${MINECRAFT_VERSION}"
     if [ -z "$MANTA_USER" ]; then
+      # Not on Manta
       pkgin -y install numpy
+      DEST=".$DEST"
     fi
-    DEST="./minecraft/versions/${MINECRAFT_VERSION}"
     ;;
   * )
     echo "Unknown OS"
@@ -37,7 +39,9 @@ git clone git://github.com/overviewer/Minecraft-Overviewer.git overviewer
 
 cd overviewer
 
-# get PIL (may already exist in OS)
+# Python Image Tools
+#
+# get PIL (may already exist in OS and Manta)
 if [ ! -d "`pwd`/Imaging-1.1.7/libImaging" ]; then
   /usr/bin/curl -o imaging.tgz http://effbot.org/media/downloads/Imaging-1.1.7.tar.gz
   tar xzf imaging.tgz
@@ -50,3 +54,16 @@ python ./setup.py clean > /dev/null
 
 # build MCO
 python ./setup.py build #> /dev/null
+
+# Create config.py
+cat > /minecraft/cfg.py <<EOCONFIG
+worlds['world1'] = '/minecraft/worlds/world1'
+outputdir = "/minecraft/render/world1"
+# Try "smooth_lighting" over "lighting" for even better looking maps!
+rendermode = "smooth_lighting"
+texturepath = "/minecraft/versions/${MINECRAFT_VERSION}/${MINECRAFT_VERSION}.jar"
+renders["render1"] = {
+  'world': 'world1',
+  'title': 'The world',
+}
+EOCONFIG
