69937a82827d17d40e6aa33238891ad5930772b5
Nate Fitch <nate.fitch@joyent.com>
2013-11-25 21:47:07 -0800

    joyent/minecraft#5 Restoring config files separately.

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -9,8 +9,21 @@ source $(dirname $0)/common.sh
 BACKUP_FILE='/var/tmp/minecraft-backup.tar.gz'
 
 if mget -o $BACKUP_FILE $REMOTE_FILE 2>/dev/null; then
+  #Unpack what we pulled down first.
   cd $MINECRAFT_LOCATION
   tar -xzf $BACKUP_FILE
+
+  #Now download the other files on top of what we got- we treat those as the
+  # latest.
+  if mls $REMOTE_LOCATION/server.properties; then
+    mget -o $MINECRAFT_LOCATION/server.properties $REMOTE_LOCATION/server.properties
+  fi
+  if mls $REMOTE_LOCATION/server.config; then
+    mget -o $MINECRAFT_LOCATION/server.config $REMOTE_LOCATION/server.config
+  fi
+  if mls $REMOTE_LOCATION/white-list.txt; then
+    mget -o $MINECRAFT_LOCATION/white-list.txt $REMOTE_LOCATION/white-list.txt
+  fi
   chown -R minecraft:minecraft $MINECRAFT_LOCATION
 else
   echo "Couldn't fetch backup from Manta."
