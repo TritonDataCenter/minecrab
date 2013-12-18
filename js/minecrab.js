fe8609b232eba1ae7880f22e5c0d184aeea6ec6e
Drew <drew666@gmail.com>
2013-12-18 11:31:12 -0800

    scroll spy updates navbar

--- js/minecrab.js
+++ js/minecrab.js
@@ -3,4 +3,7 @@ $(function() {
   $('.navbar-collapse a').click(function() {
     $('.navbar-collapse').collapse('hide');
   });
+
+  // navbar watches scroll position
+  $('body').scrollspy({ target: '.navbar-collapse', offset: 50 })
 });
